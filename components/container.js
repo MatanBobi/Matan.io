e315f4e3482315523bb5021aace4b4ff1447fcb0
Matan Borenkraout <matanbobi@gmail.com>
2021-11-27 23:40:09 +0200

    chore: more mobile fixes

--- components/container.js
+++ components/container.js
@@ -1,6 +1,10 @@
 export default function Container({ children, className }) {
   return (
-    <div className={`${className ? className : ""} container mx-auto px-14`}>
+    <div
+      className={`${
+        className ? className : ""
+      } container mx-auto px-6 md:px-14`}
+    >
       {children}
     </div>
   );
