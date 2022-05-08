d2b81b3b67a65fb83dfe039d27e87494150fa094
Matan Borenkraout <matanbobi@gmail.com>
2022-05-08 22:08:58 +0300

    fix: set hardcoded url for social

--- components/PostSocial/PostSeo.jsx
+++ components/PostSocial/PostSeo.jsx
@@ -1,9 +1,24 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { globals } from "../../lib/constants";
 
 export const PostSeo = ({ title, description, coverImage, keywords }) => {
+  const router = useRouter();
+  const _pathSliceLength = Math.min.apply(Math, [
+    router.asPath.indexOf("?") > 0
+      ? router.asPath.indexOf("?")
+      : router.asPath.length,
+    router.asPath.indexOf("#") > 0
+      ? router.asPath.indexOf("#")
+      : router.asPath.length,
+  ]);
+  const canonicalURL =
+    globals.siteUrl + router.asPath.substring(0, _pathSliceLength);
+
   return (
     <Head>
       <title>{title} | Matan.io</title>
+      <link rel="canonical" href={canonicalURL} />
       <meta property="og:type" content="article" />
       <meta property="og:title" content={title} />
       {description && (
@@ -13,15 +28,15 @@ export const PostSeo = ({ title, description, coverImage, keywords }) => {
           <meta name="twitter:description" content={description} />
         </>
       )}
-      <meta property="og:image" content={coverImage} />
+      <meta property="og:image" content={`${globals.siteUrl}${coverImage}`} />
       <meta
         name="twitter:card"
         content={coverImage ? "summary_large_image" : "summary"}
       />
       {keywords && <meta name="keywords" content={keywords} />}
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:image" content={coverImage} />
-      <meta name="image" content={coverImage} />
+      <meta name="twitter:image" content={`${globals.siteUrl}${coverImage}`} />
+      <meta name="image" content={`${globals.siteUrl}${coverImage}`} />
     </Head>
   );
 };
