9497ec100071e3b51cd731c79f36d5a14a3583a8
Matan Borenkraout <mborenkraout@microsoft.com>
2022-10-18 09:27:58 +0300

    feat: link animation

--- components/MDX/index.js
+++ components/MDX/index.js
@@ -1,11 +1,25 @@
 import { MarkdownCodeBlock } from "./MarkdownCodeBlock";
 import { MarkdownImage } from "./MarkdownImage";
 
+const Link = (props) => {
+  return (
+    <a
+      className="markdown-link"
+      href={props.href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {props.children}
+    </a>
+  );
+};
+
 export const components = {
   img: MarkdownImage,
   // h1: Heading.H1,
   // h2: Heading.H2,
   // p: Text,
+  a: Link,
   code: MarkdownCodeBlock,
   // inlineCode: Code
 };
