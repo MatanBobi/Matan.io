c65696b31acc035a5d0b8c4205fa7bbe4f93ea84
Matan Borenkraout <matanbobi@gmail.com>
2023-06-03 12:29:13 +0300

    chore: fix favicon

--- components/meta.js
+++ components/meta.js
@@ -9,31 +9,17 @@ export default function Meta() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
         rel="apple-touch-icon"
-        sizes="180x180"
-        href="data:image/svg+xml,&#x3C;svg xmlns=&#x22;http://www.w3.org/2000/svg&#x22; viewBox=&#x22;0 0 100 100&#x22;&#x3E;&#x3C;text y=&#x22;.9em&#x22; font-size=&#x22;90&#x22;&#x3E;&#x1F96C;&#x3C;/text&#x3E;&#x3C;/svg&#x3E;"
+        type="image/png"
+        sizes="20x20"
+        href="favicons/favicon.png"
       />
       <link
         rel="icon"
         type="image/png"
-        sizes="32x32"
-        href="data:image/svg+xml,&#x3C;svg xmlns=&#x22;http://www.w3.org/2000/svg&#x22; viewBox=&#x22;0 0 100 100&#x22;&#x3E;&#x3C;text y=&#x22;.9em&#x22; font-size=&#x22;90&#x22;&#x3E;&#x1F96C;&#x3C;/text&#x3E;&#x3C;/svg&#x3E;"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="data:image/svg+xml,&#x3C;svg xmlns=&#x22;http://www.w3.org/2000/svg&#x22; viewBox=&#x22;0 0 100 100&#x22;&#x3E;&#x3C;text y=&#x22;.9em&#x22; font-size=&#x22;90&#x22;&#x3E;&#x1F96C;&#x3C;/text&#x3E;&#x3C;/svg&#x3E;"
+        sizes="20x20"
+        href="favicons/favicon.png"
       />
       <link rel="manifest" href="/favicons/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicons/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <link
-        rel="shortcut icon"
-        href="data:image/svg+xml,&#x3C;svg xmlns=&#x22;http://www.w3.org/2000/svg&#x22; viewBox=&#x22;0 0 100 100&#x22;&#x3E;&#x3C;text y=&#x22;.9em&#x22; font-size=&#x22;90&#x22;&#x3E;&#x1F96C;&#x3C;/text&#x3E;&#x3C;/svg&#x3E;"
-      />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
