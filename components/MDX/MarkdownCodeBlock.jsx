1cc79ff72182265a87aee8ae516cdcff1dbc317a
Matan Borenkraout <matanbobi@gmail.com>
2023-08-06 16:20:29 +0300

    add making the most out of copilot post

--- components/MDX/MarkdownCodeBlock.jsx
+++ components/MDX/MarkdownCodeBlock.jsx
@@ -1,8 +1,19 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { nord } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-export const MarkdownCodeBlock = ({ className, ...props }) => {
+function replaceLast(str, find, replace) {
+  const lastIndex = str.lastIndexOf(find);
+  if (lastIndex === -1) {
+    return str;
+  }
+  const before = str.substring(0, lastIndex);
+  const after = str.substring(lastIndex + find.length);
+  return before + replace + after;
+}
+
+export const MarkdownCodeBlock = ({ className, children, ...props }) => {
   const match = /language-(\w+)/.exec(className || "");
+  const formattedChildren = replaceLast(children, "\n", "");
   return match ? (
     <SyntaxHighlighter
       language={match[1]}
@@ -10,7 +21,9 @@ export const MarkdownCodeBlock = ({ className, ...props }) => {
       customStyle={{ margin: 0 }}
       style={nord}
       {...props}
-    />
+    >
+      {formattedChildren}
+    </SyntaxHighlighter>
   ) : (
     <code className={className} {...props} />
   );
