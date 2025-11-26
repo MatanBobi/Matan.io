a9d06f6198b0a409cceeb0f918efb8dfc9d9f857
Matan Borenkraout <matanbobi@gmail.com>
2025-11-26 09:46:12 +0200

    fix cursor position

--- components/Logo/index.jsx
+++ components/Logo/index.jsx
@@ -2,7 +2,7 @@ export const Logo = () => {
   return (
     <div className="flex md:m-9 text-3xl font-logo items-end">
       <span>bobi</span>
-      <div className="bg-cyan w-4 h-1 my-2.5 mx-1 rounded-sm animate-fade"></div>
+      <div className="bg-cyan w-4 h-1 my-2 mx-1 rounded-sm animate-fade"></div>
     </div>
   );
 };
